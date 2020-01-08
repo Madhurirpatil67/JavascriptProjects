c4f80932ed39e7277e8e772d235481fa23333a1e
Madhurirpatil67 <madhurisankpal45@gmail.com>
2020-01-08 17:50:20 +0530

    datatype

---
+++ main.js
@@ -0,0 +1,40 @@
+
+/*
+primitives data types- number,boolean,string
+composite/reference data types-array,object,function
+special data types-null,undefined
+*/
+//typeof operator gives data type of variables
+
+//number
+var num1=10;
+console.log(typeof(num1)); //number
+
+//string
+var str1="madhu";
+console.log(typeof(str1)); //string
+
+//boolean
+var bool=true;
+console.log(typeof(bool)); //boolean
+
+//null
+let x=null;
+console.log(x);//null
+console.log(typeof(x)); //object
+
+//undefined
+let y;
+console.log(typeof(y)); //undefined
+
+//array
+var arr=[1,2,3,4,5];
+console.log(typeof(arr)); //object
+
+//object
+let obj={};
+console.log(typeof(obj)); //object
+
+//function
+function demo(){}
+console.log(typeof(demo)); //function
