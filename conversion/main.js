fdbcbe81d73d68f416abdfe20cdb7cae12a252af
Madhurirpatil67 <madhurisankpal45@gmail.com>
2020-01-16 23:08:06 +0530

    conversions

---
+++ conversion/main.js
@@ -0,0 +1,64 @@
+//Coversion
+
+// Number
+let num1=10;
+console.log(String(num1)); // 10
+console.log(Boolean(num1)); //true
+
+let num2=0;
+console.log(String(num2)); // 0
+console.log(Boolean(num2)); //false
+
+let num3=0.5;
+console.log(String(num3)); // 0.5
+console.log(Boolean(num3)); //true
+
+let num4=-20;
+console.log(String(num4)); // -20
+console.log(Boolean(num4)); //true
+
+//String
+let str1="10";
+console.log(Number(str1)); // 10
+console.log(Boolean(str1)); //true
+
+let str2="Hello";
+console.log(Number(str2)); // NAN
+console.log(Boolean(str2)); //true
+
+let str3="";
+console.log(Number(str3)); // 0
+console.log(Boolean(str3)); //false
+
+let str4=" ";
+console.log(Number(str4)); // 0
+console.log(Boolean(str4)); //true
+
+let str5="0";
+console.log(Number(str5)); // 0
+console.log(Boolean(str5)); //true
+
+//Boolean
+let bool=true;
+console.log(Number(bool)); // 1
+console.log(String(bool)); //true
+
+let bool1=false;
+console.log(Number(bool1)); // 0
+console.log(String(bool1)); //false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
